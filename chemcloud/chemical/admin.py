b9df3bbc46b21c4a3dedac5def64d3599de7f2f2
KateGL <yolkate@gmail.com>
2016-03-09 17:30:55 +0500

    Атом, Вещество, реакция - начало

--- chemcloud/chemical/admin.py
+++ chemcloud/chemical/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from chemical.models import Atom, Substance, Reaction
+
+class AtomAdmin(admin.ModelAdmin):
+  fields = ['atom_number', 'symbol','atom_mass',  'name', 'name_latin']
+  list_display = ('atom_number', 'symbol', 'atom_mass', 'name', 'name_latin')
+
+admin.site.register(Atom, AtomAdmin)
+
+
+class SubstanceAdmin(admin.ModelAdmin):
+  fields = ['name', 'charge', 'is_radical', 'formula_brutto']
+  list_display = ('id_substance',  'name',  'charge', 'is_radical', 'formula_brutto')
+
+admin.site.register(Substance, SubstanceAdmin)
+
+class ReactionAdmin(admin.ModelAdmin):
+  fields = ['name',  'is_favorite', 'is_stationary', 'is_isothermal' ]
+  list_display = ('id_reaction',  'name', 'is_favorite', 'is_stationary', 'is_isothermal' )
+
+admin.site.register(Reaction, ReactionAdmin)
--- chemcloud/chemical/models.py
+++ chemcloud/chemical/models.py
@@ -3,3 +3,45 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Atom(models.Model): 
+    atom_number = models.IntegerField(primary_key = True)
+    symbol = models.CharField(max_length=3, unique=True)
+    atom_mass = models.DecimalField(max_digits=11, decimal_places=7)
+    name = models.CharField(max_length=100, unique=True)
+    name_latin = models.CharField(max_length=100, unique=True) 
+ 
+
+class Substance(models.Model): 
+    id_substance = models.AutoField(primary_key = True)
+    name = models.CharField(max_length=255)
+    charge = models.SmallIntegerField (default = 0)
+    is_radical = models.BooleanField(default = False)
+    formula_brutto = models.CharField(max_length=255)
+    #note = models.TextField()
+    #formula_mol = models.FileField()
+    #formula_picture = models.ImageField()
+
+#
+
+#
+class Reaction(models.Model):
+    id_reaction = models.AutoField(primary_key = True)
+    name = models.CharField(max_length=300)
+    #description = models.TextField()
+    is_favorite = models.BooleanField(default = False)
+    is_stationary = models.BooleanField(default = True)
+    is_isothermal = models.BooleanField(default = True)
+    
+
+#
+
+#
+
+
+
+
+
+
+ 
