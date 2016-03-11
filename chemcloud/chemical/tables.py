4aad5e2c979a9b495c92d5635e733638ac5df85a
KateGL <yolkate@gmail.com>
2016-03-11 23:54:00 +0500

    Тааблицы, детали атома, таблица веществ, ссылки на детали вещества

--- chemcloud/chemical/tables.py
+++ chemcloud/chemical/tables.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 import django_tables2 as tables
-from chemical.models import Atom
+from django_tables2.utils import A  # alias for Accessor
+from django.utils.safestring import mark_safe
+from django.utils.html import escape
+
+from chemical.models import Atom, Substance
 
 class AtomTable(tables.Table):
+    detail_link = tables.LinkColumn('atom_detail', args=[A('pk')], orderable=False,  verbose_name='Ссылка', empty_values=())
+
+    def render_detail_link(self,record):
+        return mark_safe( ''' <a href="/atom/detail/%d">Детали</a>'''%record.pk)
+
     class Meta:
         model = Atom
         # add class="paleblue" to <table> tag
         attrs = {"class": "paleblue"}
         sequence = ("atom_number", "symbol", "name", "name_latin", "atom_mass")
+
+class SubstanceTable(tables.Table):
+    detail_link = tables.LinkColumn('substance_detail', args=[A('pk')], orderable=False,  verbose_name='Ссылка', empty_values=())
+
+    def render_detail_link(self,record):
+        return mark_safe( ''' <a href="/substance/detail/%d">Детали</a>'''%record.pk)
+
+    class Meta:
+        model = Substance
+        # add class="paleblue" to <table> tag
+        attrs = {"class": "paleblue"}
+        sequence = ("name", "formula_brutto", "charge", "is_radical")
+
--- chemcloud/chemical/views.py
+++ chemcloud/chemical/views.py
@@ -12,16 +12,28 @@ from django.contrib.auth import logout
 
 from django_tables2   import RequestConfig
 
-from chemical.models import Atom
-from chemical.tables  import AtomTable
+from chemical.models import Atom, Substance
+from chemical.tables  import AtomTable, SubstanceTable
 
 @login_required
 def reactions_all(request):
     return render(request, 'chemical/reactions_all.html', {})
 
 @login_required
-def substance_dict(request):
-    return render(request, 'chemical/substance_dict.html', {})
+def substance_all(request):
+    substance_table = SubstanceTable(Substance.objects.all())
+    RequestConfig(request, paginate={"per_page": 5}).configure(substance_table)
+    return render(request, 'chemical/substance_all.html',  {"substance": substance_table})
+
+
+@login_required
+def substance_detail(request, id_substance):
+    try:
+        substance = Substance.objects.get(pk=id_substance)
+    except Substance.DoesNotExist:
+        raise Http404("Substance does not exist")
+    return render(request, 'chemical/substance_detail.html', {"substance": substance})
+
 
 @login_required
 def calculations_all(request):
@@ -32,6 +44,15 @@ def atoms_all(request):
     atom_table = AtomTable(Atom.objects.all())
     RequestConfig(request, paginate={"per_page": 5}).configure(atom_table)
     return render(request, 'chemical/atoms_all.html',  {"atom": atom_table})
+
+@login_required
+def atom_detail(request, atom_number):
+    try:
+        atom = Atom.objects.get(pk=atom_number)
+    except Atom.DoesNotExist:
+        raise Http404("Atom does not exist")
+    return render(request, 'chemical/atom_detail.html', {"atom": atom})
+
 
 @login_required
 def dictionaries(request):
