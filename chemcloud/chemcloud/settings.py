b6dd3fb3631e608920c5ea0161f3fa51c61b1e62
KateGL <yolkate@gmail.com>
2016-03-12 22:07:14 +0500

    Install django-crispy-forms

--- chemcloud/chemcloud/settings.py
+++ chemcloud/chemcloud/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'registration', # add in the registration package  django-registration-redux
     'django_tables2',
+    'crispy_forms',
     'chemmain', # main pages of site
     'chemical', #models and views of main chemical entyties: atom, substance, reaction, mechanism, etc
 ]
